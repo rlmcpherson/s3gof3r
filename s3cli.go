0169e8226aeced0d2756970bfe973f094456f8a1
Randall McPherson <rlmcpherson@gmail.com>
2013-08-07 07:36:00 -0400

    Create s3cli package.
    Add main.go

--- s3cli.go
+++ s3cli.go
@@ -1,63 +1,16 @@
-package main
+package s3cli
 
 import (
     "github.com/rlmcpherson/s3/s3util"
     "os"
     "io"
     "fmt"
-    "strings"
-    "github.com/jessevdk/go-flags"
     "net/http"
-    "log"
     "crypto/md5"
-
 )
 
-func main() {
-
-    // Parse flags
-    args, err := flags.Parse(&opts)
-    fmt.Printf( strings.Join(args, " "))
-
-    if err != nil {
-        os.Exit(1)
-
-    }
-    s3util.DefaultConfig.AccessKey = os.Getenv("S3_ACCESS_KEY")
-    s3util.DefaultConfig.SecretKey = os.Getenv("S3_SECRET_KEY")
-
-    if opts.Down && !opts.Up{
-        err := download(opts.Url)
-        if err != nil {
-            fmt.Fprintln(os.Stderr, err)
-        }
-    }    else if opts.Up{
-        err := upload(opts.Url, opts.Header, opts.Check)
-        if err != nil {
-            fmt.Fprintln(os.Stderr, err)
-        }
-
-    } else{
-        log.Fatal("specify direction of transfer: up or down")
-    }
-
-}
-
-var opts struct {
-
-    //AccessKey string `short:"k" long:"accesskey" description:"AWS Access Key" required:"true"`
-    //SecretKey string `short:"s" long:"secretkey" description:"AWS Secret Key" required:"true"`
-    Up bool `long:"up" description:"Upload to S3"`
-    Down bool `long:"down" description:"Download from S3"`
-    FilePath string `short:"f" long:"file_path" description:"canonical path to file" required:"true"`
-    Url string `short:"u" long:"url" description:"Url of S3 object" required:"true"` 
-    Header http.Header `short:"h" long:"headers" description:"HTTP headers"` 
-    Check bool `short:"c" long:"checksum" description:"Verify integrity with  md5 checksum"`
-
-}
-
-func upload(url string, header http.Header, check bool) (error) {
-    r, err := os.Open(opts.FilePath) 
+func Upload(url string, file_path string, header http.Header, check bool) (error) {
+    r, err := os.Open(file_path) 
     if err != nil{
         return err
     }
@@ -77,12 +30,12 @@ func upload(url string, header http.Header, check bool) (error) {
     return nil
 }
 
-func download(url string) (error){
-    r, err := s3util.Open(opts.Url, nil) 
+func Download(url string, file_path string) (error){
+    r, err := s3util.Open(url, nil) 
     if err != nil{
         return err
     }
-    w, err := os.Create(opts.FilePath)
+    w, err := os.Create(file_path)
     if err != nil{
         return err
     }
@@ -100,5 +53,4 @@ func checksum(r io.Reader)(string, error){
     h:= md5.New()
     io.Copy(h, r)
  return fmt.Sprintf("%x", h.Sum(nil)), nil
-//return URLEncoding.EncodeToString(h.Sum(nil)), nil
 }
