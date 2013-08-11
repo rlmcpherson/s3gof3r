9dc8d6860365af03b9c77a8e40d16c82cf328177
Randall McPherson <rlmcpherson@gmail.com>
2013-08-11 09:41:23 -0400

    Wip. Upload hashes working.

--- s3gof3r.go
+++ s3gof3r.go
@@ -4,7 +4,8 @@ package s3gof3r
 
 import (
 	"crypto/md5"
-	"encoding/base64"
+	//"encoding/hex"
+	"fmt"
 	"github.com/rlmcpherson/s3/s3util"
 	"io"
 	"net/http"
@@ -17,11 +18,13 @@ func Upload(url string, file_path string, header http.Header, check bool) error 
 		return err
 	}
 	if check {
-		content_checksum, err := checksum(r)
+		md5hash, err := md5hash(io.ReadSeeker(r))
 		if err != nil {
 			return err
 		}
-		header.Add("x-amz-meta-checksum", content_checksum)
+		fmt.Println(md5hash)
+		header.Add("x-amz-meta-md5-hash", md5hash)
+		header.Write(os.Stdout)
 
 	}
 	w, err := s3util.Create(url, header, nil)
@@ -34,8 +37,8 @@ func Upload(url string, file_path string, header http.Header, check bool) error 
 	return nil
 }
 
-func Download(url string, file_path string) error {
-	r, err := s3util.Open(url, nil)
+func Download(url string, file_path string, check bool) error {
+	r, header, err := s3util.Open(url, nil)
 	if err != nil {
 		return err
 	}
@@ -43,6 +46,21 @@ func Download(url string, file_path string) error {
 	if err != nil {
 		return err
 	}
+	if check {
+		remoteHash := header.Get("x-amz-meta-md5-hash")
+		if remoteHash == "" {
+			return fmt.Errorf("Could not verify content. Http header 'Md5-Hash' header not found.")
+		}
+
+		//calculatedHash, err := md5hash(io.ReadSeeker(r.Read))
+		if err != nil {
+			return err
+		}
+		fmt.Println(md5hash)
+		header.Write(os.Stdout)
+
+	}
+
 	if err := fileCopyClose(w, r); err != nil {
 		return err
 	}
@@ -59,9 +77,11 @@ func fileCopyClose(w io.WriteCloser, r io.ReadCloser) error {
 	return nil
 }
 
-func checksum(r io.Reader) (string, error) {
+func md5hash(r io.ReadSeeker) (string, error) {
 	h := md5.New()
 	io.Copy(h, r)
-	//return fmt.Sprintf("%x", h.Sum(nil)), nil
-	return base64.StdEncoding.EncodeToString(h.Sum(nil)), nil
+	r.Seek(0, 0)
+	//encoder := base64.NewEncoder(base64.StdEncoding, b64)
+	return (fmt.Sprintf("%x", h.Sum(nil))), nil
+	//return hex.EncodeToString(h.Sum(nil)), nil
 }
