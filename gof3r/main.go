4a5b21575ab66b03ea1af7329837f06176edf669
Randall McPherson <rlmcpherson@gmail.com>
2013-08-18 18:31:40 -0400

    Piping downloads.

--- gof3r/main.go
+++ gof3r/main.go
@@ -42,6 +42,7 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -56,6 +57,8 @@ func main() {
 	}
 	s3util.DefaultConfig.AccessKey = os.Getenv("AWS_ACCESS_KEY")
 	s3util.DefaultConfig.SecretKey = os.Getenv("AWS_SECRET_KEY")
+
+	start := time.Now()
 
 	if opts.Down && !opts.Up {
 		err := s3gof3r.Download(opts.Url, opts.FilePath, opts.Check)
@@ -73,6 +76,7 @@ func main() {
 	} else {
 		log.Fatal("specify direction of transfer: up or down")
 	}
+	log.Println("Duration:", time.Since(start))
 	if opts.Debug {
 		panic("Dump the stacks.")
 	}
@@ -85,7 +89,7 @@ var opts struct {
 	//SecretKey string `short:"s" long:"secretkey" description:"AWS Secret Key"`
 	Up       bool        `long:"up" description:"Upload to S3"`
 	Down     bool        `long:"down" description:"Download from S3"`
-	FilePath string      `short:"f" long:"file_path" description:"canonical path to file" required:"true"`
+	FilePath string      `short:"f" long:"file_path" description:"Path to file. Stdout / Stdin are used if not specified. "`
 	Url      string      `short:"u" long:"url" description:"Url of S3 object" required:"true"`
 	Header   http.Header `short:"h" long:"headers" description:"HTTP headers"`
 	Check    bool        `short:"c" long:"md5-checking" description:"Use md5 hash checking to ensure data integrity."`
--- s3gof3r.go
+++ s3gof3r.go
@@ -6,6 +6,7 @@ import (
 	"crypto/md5"
 	"fmt"
 	//"github.com/op/go-logging"
+	//	"archive/tar"
 	"bufio"
 	"github.com/rlmcpherson/s3/s3util"
 	"io"
@@ -23,10 +24,8 @@ func Upload(url string, file_path string, header http.Header, check bool) error 
 	if err != nil {
 		return err
 	}
-
 	br := bufio.NewReader(r)
 	defer r.Close()
-
 	if check {
 		md5hash, err := md5hash(file_path)
 		if err != nil {
@@ -37,7 +36,7 @@ func Upload(url string, file_path string, header http.Header, check bool) error 
 		}
 		header.Set(checkSumHeader, md5hash)
 		log.Println("POST REQ HEADER:")
-		header.Write(os.Stdout)
+		header.Write(os.Stderr)
 	}
 	w, err := s3util.Create(url, header, nil)
 	if err != nil {
@@ -46,9 +45,8 @@ func Upload(url string, file_path string, header http.Header, check bool) error 
 	if _, err := io.Copy(w, br); err != nil {
 		return err
 	}
-	if err := w.Close(); err != nil {
-		return err
-	}
+
+	err = w.Close()
 	return nil
 }
 
@@ -60,22 +58,33 @@ func Download(url string, file_path string, check bool) error {
 	defer r.Close()
 	w, err := os.Create(file_path)
 	if err != nil {
-		return err
+		if file_path == "" {
+			w = os.Stdout
+		} else {
+			return err
+		}
 	}
 	defer w.Close()
+
 	if check {
 		h := md5.New()
+
+		// buffered to reduce disk IO
+		bh := bufio.NewWriter(h)
 		bw := bufio.NewWriter(w)
-		mw := io.MultiWriter(bw, h)
+		mw := io.MultiWriter(bw, bh)
 		if _, err := io.Copy(mw, r); err != nil {
 			return err
 		}
+		// flush buffers to ensure all data is copied
 		bw.Flush()
+		bh.Flush()
+
 		calculatedHash := fmt.Sprintf("%x", h.Sum(nil))
 		log.Println("Calculated MD5 Hash:", calculatedHash)
 		remoteHash := header.Get(checkSumHeader)
 		log.Println("GET REQ HEADER:")
-		header.Write(os.Stdout)
+		header.Write(os.Stderr)
 		if remoteHash == "" {
 			return fmt.Errorf("Could not verify content. Http header %s not found.", checkSumHeader)
 		}
