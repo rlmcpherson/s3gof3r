722a0e0301931696b9e8b64b857830348cfccdf7
Randall McPherson <rlmcpherson@gmail.com>
2014-03-03 19:46:56 -0500

    Documentation updates and formatting.

--- s3gof3r.go
+++ s3gof3r.go
@@ -10,26 +10,31 @@ import (
 	"time"
 )
 
+// S3 contains the domain or endpoint of an S3-compatible service and
+// the authentication keys for that service.
 type S3 struct {
-	Domain string // The s3-compatible service domain. Defaults to "s3.amazonaws.com"
+	Domain string // The s3-compatible endpoint. Defaults to "s3.amazonaws.com"
 	Keys
 }
 
+// A Bucket for an S3 service.
 type Bucket struct {
 	*S3
 	Name string
 }
 
+// Config includes configuration parameters for s3gof3r
 type Config struct {
-	*http.Client        // nil to use s3gof3r default client
-	Concurrency  int    // number of parts to get or put concurrently
-	PartSize     int64  //  initial  part size in bytes to use for multipart gets or puts
-	NTry         int    // maximum attempts for each part
-	Md5Check     bool   // the md5 hash of the object is stored in <bucket>/.md5/<object_key> and verified on gets
-	Scheme       string // url scheme, defaults to 'https'
+	*http.Client       // nil to use s3gof3r default client
+	Concurrency  int   // number of parts to get or put concurrently
+	PartSize     int64 // initial  part size in bytes to use for multipart gets or puts
+	NTry         int   // maximum attempts for each part
+	Md5Check     bool  // The md5 hash of the object is stored in <bucket>/.md5/<object_key>.md5
+	// When true, it is stored on puts and verified on gets
+	Scheme string // url scheme, defaults to 'https'
 }
 
-// Defaults
+// DefaultConfig contains defaults used if *Config is nil
 var DefaultConfig = &Config{
 	Concurrency: 10,
 	PartSize:    20 * mb,
@@ -43,9 +48,10 @@ const (
 	clientTimeout = 5 * time.Second
 )
 
+// DefaultDomain is set to the endpoint for the U.S. S3 service.
 var DefaultDomain = "s3.amazonaws.com"
 
-// Returns a new S3
+// New Returns a new S3
 // domain defaults to DefaultDomain if empty
 func New(domain string, keys Keys) *S3 {
 	if domain == "" {
@@ -54,16 +60,17 @@ func New(domain string, keys Keys) *S3 {
 	return &S3{domain, keys}
 }
 
-// Returns a bucket on s3j
+// Bucket returns a bucket on s3
 func (s3 *S3) Bucket(name string) *Bucket {
 	return &Bucket{s3, name}
 }
 
-// Provides a reader and downloads data using parallel ranged get requests.
-// Data from the requests is reordered and written sequentially.
+// GetReader provides a reader and downloads data using parallel ranged get requests.
+// Data from the requests are ordered and written sequentially.
 //
 // Data integrity is verified via the option specified in c.
 // Header data from the downloaded object is also returned, useful for reading object metadata.
+// DefaultConfig is used if c is nil
 func (b *Bucket) GetReader(path string, c *Config) (r io.ReadCloser, h http.Header, err error) {
 	if c == nil {
 		c = DefaultConfig
@@ -74,7 +81,7 @@ func (b *Bucket) GetReader(path string, c *Config) (r io.ReadCloser, h http.Head
 	return newGetter(b.Url(path, c), c, b)
 }
 
-// Provides a writer to upload data as multipart upload requests.
+// PutWriter provides a writer to upload data as multipart upload requests.
 //
 // Each header in h is added to the HTTP request header. This is useful for specifying
 // options such as server-side encryption in metadata as well as custom user metadata.
@@ -89,7 +96,7 @@ func (b *Bucket) PutWriter(path string, h http.Header, c *Config) (w io.WriteClo
 	return newPutter(b.Url(path, c), h, c, b)
 }
 
-// Returns a parsed url to the given path, using the scheme specified in Config.Scheme
+// Url returns a parsed url to the given path, using the scheme specified in Config.Scheme
 func (b *Bucket) Url(path string, c *Config) url.URL {
 	url_, err := url.Parse(fmt.Sprintf("%s://%s.%s/%s", c.Scheme, b.Name, b.S3.Domain, path))
 	if err != nil {
