5b7b35b579a2e163c6414f309d401a96013dbbe9
Randall McPherson <rlmcpherson@gmail.com>
2014-06-20 14:05:46 -0400

    Remove gocheck test dependency.

--- auth_test.go
+++ auth_test.go
@@ -4,11 +4,7 @@ import (
 	"os"
 	"strings"
 	"testing"
-	. "launchpad.net/gocheck"
 )
-
-// Hook up gocheck into the "go test" runner.
-func Test(t *testing.T) { TestingT(t) }
 
 var testKeys = Keys{
 	AccessKey: "AKIDEXAMPLE",
@@ -21,23 +17,18 @@ var testTokenKeys = Keys{
 	SecurityToken: "testtoken",
 }
 
-type AuthT struct {
+type authT struct {
 	env []string
 }
 
-var _ = Suite(&AuthT{})
-
 // save current environment for restoration
-func (s *AuthT) SetUpSuite(c *C) {
+func (s *authT) saveEnv() {
 	s.env = os.Environ()
-}
-
-func (s *AuthT) SetUpTest(c *C) {
 	os.Clearenv()
 }
 
 // restore environment after each test
-func (s *AuthT) TearDownTest(c *C) {
+func (s *authT) restoreEnv() {
 	os.Clearenv()
 	for _, kv := range s.env {
 		l := strings.SplitN(kv, "=", 2)
@@ -45,15 +36,28 @@ func (s *AuthT) TearDownTest(c *C) {
 	}
 }
 
-func (s *AuthT) TestEnvKeys(c *C) {
+func TestEnvKeys(t *testing.T) {
+	s := authT{}
+	s.saveEnv()
 	os.Setenv("AWS_ACCESS_KEY_ID", testKeys.AccessKey)
 	os.Setenv("AWS_SECRET_ACCESS_KEY", testKeys.SecretKey)
 	keys, err := EnvKeys()
-	c.Assert(err, IsNil)
-	c.Assert(keys, Equals, testKeys)
+	if err != nil {
+		t.Error(err)
+	}
+	if keys != testKeys {
+		t.Errorf("Keys do not match. Expected: %v. Actual: %v", testKeys, keys)
+	}
+	s.restoreEnv()
 }
 
-func (s *AuthT) TestEnvKeysNotSet(c *C) {
+func TestEnvKeysNotSet(t *testing.T) {
+	s := authT{}
+	s.saveEnv()
 	_, err := EnvKeys()
-	c.Assert(err, ErrorMatches, "Keys not set in environment: AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY")
+	expErr := "Keys not set in environment: AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY"
+	if err.Error() != expErr {
+		t.Errorf("Expected error: %v. Actual: %v", expErr, err)
+	}
+	s.restoreEnv()
 }
